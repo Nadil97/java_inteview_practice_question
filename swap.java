64a4c5b2e03a5acb4571e2c64491cf2d5872c461
nadil sa <nadilsandaruwan97@gmail.com>
2023-02-18 11:32:32 +0530

    finish swaping program

--- swap.java
+++ swap.java
@@ -1,4 +1,16 @@
-pubilc class swap{
-    
+class swap{
+    public static void main(String[] args) {
+        int a= 5;
+        int b = 10;
+
+        System.out.println("without swap a is= "+a+" b is ="+b);
+
+        a =a+b;
+        b =a-b;
+        a =a-b;
+
+        System.out.println("After swaping a= "+a+ " B is after swaping= "+b);
+    }
+
 
 }
